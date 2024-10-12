4ef360f3bf8a184c80121a2c0e0300f93059c9b5
anna putilovskaya <putilovskayaam@gmail.com>
2024-10-12 14:53:28 +0300

    create DatabaseHelper

--- cadastre_api/config/config.py
+++ cadastre_api/config/config.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from pydantic import PostgresDsn
 from pydantic_settings import  BaseSettings
 
 
@@ -7,8 +8,18 @@ class RunConfig(BaseModel):
     port: int = 8000
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False,
+    echo_pool: bool = False,
+    pool_pre_ping: bool = True,
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):
     run:  RunConfig = RunConfig()
+    db: DatabaseConfig = DatabaseConfig()
 
 
 settings = Settings()

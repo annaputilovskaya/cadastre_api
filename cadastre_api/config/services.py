6ac9ed881660fba0ea15391d289f1018cd16da2c
anna putilovskaya <putilovskayaam@gmail.com>
2024-10-12 21:58:10 +0300

    create convert_camelcase_to_snakecase function

---
+++ cadastre_api/config/services.py
@@ -0,0 +1,16 @@
+def convert_camelcase_to_snakecase(input_str: str) -> str:
+    """
+    Преобразует стиль написания из верблюжьего в змеиный.
+    """
+    chars = []
+    for c_idx, char in enumerate(input_str):
+        if c_idx and char.isupper():
+            nxt_idx = c_idx + 1
+            flag = nxt_idx >= len(input_str) or input_str[nxt_idx].isupper()
+            prev_char = input_str[c_idx - 1]
+            if prev_char.isupper() and flag:
+                pass
+            else:
+                chars.append("_")
+        chars.append(char.lower())
+    return "".join(chars)

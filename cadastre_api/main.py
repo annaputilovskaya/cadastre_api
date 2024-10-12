b6eb8139d8f428770d04266fb3d077f5182b0aa6
anna putilovskaya <putilovskayaam@gmail.com>
2024-10-12 11:58:40 +0300

    include queries_router and users_router to main.py

--- cadastre_api/main.py
+++ cadastre_api/main.py
@@ -1,10 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
 
+from config.config import settings
+
+from queries import router as queries_router
+from users import router as users_router
+
 app = FastAPI(
     title="Cadastre API"
 )
 
+app.include_router(queries_router)
+app.include_router(users_router)
+
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True)
